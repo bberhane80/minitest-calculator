c307237a8ae9894514f8bcf5b288d712c5c90a0f
Berhane Berhane <berhane896@gmail.com>
2025-03-22 01:59:15 +0000

    added classes and tested two different functions

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,5 +1,31 @@
 class Calculator 
     def add(a,b)
         a + b
+    end
+
+    def subtract(a,b)
+        a - b
+    end
+end
+
+
+
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+    def test_addition
+        calculator = Calculator.new
+        assert_equal 4, calculator.add(2,2), "Addition method failed"
     end 
+end
+
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+    def test_subtraction
+        calculator = Calculator.new
+        assert_equal 0, calculator.subtract(2,2), "Subtract method failed"
+    end
 end
